9eb11dec21c4df0ada949b281ac68dea6139ddd8
mioe <mioe.boss@gmail.com>
2021-09-10 19:48:43 +0000

    Deploying to gh-pages from @ mioe/vite-js-template@1615779aff00a9bd65b3649d242a3312072a072b 🚀

---
+++ assets/Error404.ee66cf7b.js
@@ -0,0 +1,1 @@
+import{o as a,c as e,a as s}from"./vendor.6e45ed10.js";const t={},r={class:"relative"},n=[s("div",{class:"container mx-auto pt-[10px] px-[16px]"},[s("h1",{class:"text-2xl"}," 404 ")],-1)];t.render=function(s,t){return a(),e("main",r,n)};export{t as default};
